97832731a945465106abd61d8bbc4f2cf401bf22
PipsChips <kvesic_ante@yahoo.com>
2017-12-05 11:50:33 +0000

    refactor app.js

--- app.js
+++ app.js
@@ -18,42 +18,44 @@ var campgroundRoutes = require("./routes/campgrounds.js");
 var commentRoutes = require("./routes/comments.js");
 var indexRoutes = require("./routes/index.js");
 
+var url = process.env.DATABASEURL || "mongodb://localhost/yelpCamp";
+mongoose.connect(url, {useMongoClient: true});
+
 // Configuring application
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// Passport Configuration
 app.use(require("express-session")({
     secret: "yelpCampSecret",
     resave: false,
     saveUninitialized: false
 }));
-app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(function(req, res, next) {
     res.locals.currentUser = req.user;
     res.locals.error = req.flash("error");
     res.locals.success = req.flash("success");
     next();
 });
-app.use(methodOverride("_method"));
+
+//seedDb();
+
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-passport.use(new localStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-var url = process.env.DATABASEURL || "mongodb://localhost/yelpCamp";
-mongoose.connect(url, {useMongoClient: true});
-//seedDb();
-
-
 
 // ==================================
-// SERVER
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The YelpCamp server is runing ...");
